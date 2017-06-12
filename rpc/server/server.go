e5f7d38271e817a4ee819b7efb2ecb930f8793b0
taihe <taihe@bigwei.com>
2017-06-12 16:05:13 +0800

    th

--- rpc/server/server.go
+++ rpc/server/server.go
@@ -19,9 +19,30 @@ func init() {
 }
 
 func run() {
-	//服务
-	services := map[string] interface{} {
-		"Company.Cloud.Company": &models.Company{},
+	// 服务
+	services := map[string]interface{}{
+		"Account": &models.Account{},
+		"AccountItem": &models.AccountItem{},
+		"Transaction": &models.Transaction{},
+		"App": &models.Application{},
+		"AppTpl": &models.ApplicationTpl{},
+		"AttendanceGroup":&models.ApplicationTpl{},
+		"AttendanceRecord":&models.AttendanceRecord{},
+		"AttendanceSetup":&models.AttendanceSetup{},
+		"AttendanceShift":&models.AttendanceShift{},
+		"AttendanceShiftItem":&models.AttendanceShiftItem{},
+		"AttendanceShiftRecord":&models.AttendanceShiftRecord{},
+		"Card":&models.Card{},
+		"Company":&models.Company{},
+		"Logger":&models.Logger{},
+		"Machine":&models.Machine{},
+		"Order":&models.Order{},
+		"User":&models.User{},
+		"UserPosition":&models.UserPosition{},
+		"Person":&models.Person{},
+		"PersonRelation":&models.PersonRelation{},
+		"PersonStructure":&models.PersonStructure{},
+		"Structure":&models.Structure{},
 	}
 	register(services)
 }
