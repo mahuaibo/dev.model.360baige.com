2928e1cd26021d33d93202c27f300573e86e1950
mahuaibo <mahuaibo@360baige.com>
2017-07-10 15:48:19 +0800

    ma

--- models/logger/logger.go
+++ models/logger/logger.go
@@ -1,20 +1,14 @@
 package logger
 
 type Logger struct {
-	Id         int64 `db:"id" json:"id"`                   // 主键 id
-	CreateTime int64 `db:"create_time" json:"create_time"` // 创建时间
-	CompanyId  int64 `db:"company_id" json:"company_id"`   // 公司id
-	OwnerId    int64 `db:"owner_id" json:"owner_id"`       // 操作者ID
-	Remark     string `db:"remark" json:"remark"`          // 描述
-	Content    string `db:"content" json:"content"`        // 内容(修改前后变化)
-	Type       int `db:"type" json:"type"`                 // 类别（增、删、改、查）
+	 Id int64 `db:"id" json:"id"` // 主键 id
+	 CreateTime int64 `db:"create_time" json:"create_time"` // 创建时间
+	 CompanyId int64 `db:"company_id" json:"company_id"` // 企业ID
+	 UserId int64 `db:"user_id" json:"user_id"` // 操作者ID
+	 UserPositionId int64 `db:"user_position_id" json:"user_position_id"` // 操作者ID
+	 UserPositionType int8 `db:"user_position_type" json:"user_position_type"` // 类别（增、删、改、查）
+	 Content string `db:"content" json:"content"` // 内容(修改前后变化)
+	 Remark string `db:"remark" json:"remark"` // 描述
+	 Type int8 `db:"type" json:"type"` // 类别（增、删、改、查）
+	
 }
-
-// Logger 日志信息
-const (
-	LoggerTypeAdd    = 1
-	LoggerTypeUpdate = 2
-	LoggerTypeDelete = 3
-	LoggerTypeFind   = 4
-	LoggerTypeOther  = 5
-)
--- models/user/user.go
+++ models/user/user.go
@@ -1,23 +1,16 @@
 package user
 
 type User struct {
-	Id          int64 `db:"id" json:"id"`                      // 主键
-	CreateTime  int64 `db:"create_time" json:"create_time"`    // 创建时间（毫秒）
-	UpdateTime  int64 `db:"update_time" json:"update_time"`    // 修改时间（毫秒）
-	Username    string `db:"username" json:"username"`         // 用户名
-	Password    string `db:"password" json:"password"`         // 密码
-	Email       string `db:"email" json:"email"`               // 邮件
-	Phone       string `db:"phone" json:"phone"`               // 手机号
-	Status      int8 `db:"status" json:"status"`               // 0正常  1注销
-	Code        string `db:"code" json:"code"`                 // 验证码
-	CodeTime    int64 `db:"code_time" json:"code_time"`        // 验证码时效
-	AccessToken string `db:"access_token" json:"access_token"` // 访问令牌
-	ExpireIn    int `db:"expire_in" json:"expire_in"`          // 访问时效
+	Id           int64 `db:"id" json:"id,omitempty"`                        // 主键
+	CreateTime   int64 `db:"create_time" json:"create_time,omitempty"`      // 创建时间（毫秒）
+	UpdateTime   int64 `db:"update_time" json:"update_time,omitempty"`      // 修改时间（毫秒）
+	Username     string `db:"username" json:"username,omitempty"`           // 用户名
+	Password     string `db:"password" json:"password,omitempty"`           // 密码
+	Email        string `db:"email" json:"email,omitempty"`                 // 邮件
+	Phone        string `db:"phone" json:"phone,omitempty"`                 // 手机号
+	Status       int8 `db:"status" json:"status,omitempty"`                 // 状态 -1注销 0正常
+	Code         string `db:"code" json:"code,omitempty"`                   // 验证码
+	CodeTime     int64 `db:"code_time" json:"code_time,omitempty"`          // 验证码时效
+	AccessTicket string `db:"access_ticket" json:"access_ticket,omitempty"` // 访问令牌
+	ExpireIn     int64 `db:"expire_in" json:"expire_in,omitempty"`          // 访问时效
 }
-
-// User 用户状态
-const (
-	UserStatusValid   = 1
-	UserStatusInValid = 0
-	UserStatusCancel  = -1
-)
