8abdb1faaac29dede771ab0ef206ef789d3854d8
taihe <taihe@bigwei.com>
2017-07-22 12:07:45 +0800

    th

--- http/window/company.go
+++ http/window/company.go
@@ -3,24 +3,26 @@ package window
 import (
 	"dev.model.360baige.com/models/company"
 )
+
 type CompanyDetailResponse struct {
 	Code    string `json:"code"`
 	Messgae string        `json:"messgae"`
 	Data    CompanyDetail   `json:"data,omitempty"`
 }
 type CompanyDetail struct {
-	Logo string `db:"logo" json:"logo"` // 企业LOGO
-	Name string `db:"name" json:"name"` // 企业全名
-	ShortName string `db:"short_name" json:"short_name"` // 企业简称（微信端显示使用）
+	Id         int64 `db:"id" json:"id"`                   // 企业Id
+	Logo       string `db:"logo" json:"logo"`              // 企业LOGO
+	Name       string `db:"name" json:"name"`              // 企业全名
+	ShortName  string `db:"short_name" json:"short_name"`  // 企业简称（微信端显示使用）
 	ProvinceId int64 `db:"province_id" json:"province_id"` // 省
-	CityId int64 `db:"city_id" json:"city_id"` // 市
+	CityId     int64 `db:"city_id" json:"city_id"`         // 市
 	DistrictId int64 `db:"district_id" json:"district_id"` // 县
-	Address string `db:"address" json:"address"` // 企业地址
-	PositionX float64 `db:"position_x" json:"position_x"` // x
-	PositionY float64 `db:"position_y" json:"position_y"` // y
-	Remark string `db:"remark" json:"remark"` // 备注
-	Brief string `db:"brief" json:"brief"` // 简介
-	Status int8 `db:"status" json:"status"` // 1 启用 0 停用
+	Address    string `db:"address" json:"address"`        // 企业地址
+	PositionX  float64 `db:"position_x" json:"position_x"` // x
+	PositionY  float64 `db:"position_y" json:"position_y"` // y
+	Remark     string `db:"remark" json:"remark"`          // 备注
+	Brief      string `db:"brief" json:"brief"`            // 简介
+	Status     int8 `db:"status" json:"status"`            // 1 启用 0 停用
 }
 type CompanyModifyResponse struct {
 	Code    string `json:"code"`
--- http/window/user.go
+++ http/window/user.go
@@ -5,12 +5,14 @@ type UserLoginResponse struct {
 	Messgae string        `json:"messgae"`
 	Data    UserLogin   `json:"data,omitempty"`
 }
+
 type ModifyPasswordResponse struct {
 	Code    string `json:"code"`
 	Messgae string        `json:"messgae"`
 }
 
 type UserLogin struct {
+	Id           int64 `db:"username" json:"id"`
 	Username     string `db:"username" json:"username"`           // 用户名
 	AccessTicket string `db:"access_ticket" json:"access_ticket"` // 访问令牌
 	ExpireIn     int64 `db:"expire_in" json:"expire_in"`          // 访问时效
