d4aa9b5b75b75a9be27f20fb00e6439ffc7a9f2a
mahuaibo <mahuaibo@360baige.com>
2017-08-17 17:35:17 +0800

    ma

--- models/card/card.go
+++ models/card/card.go
@@ -1,11 +1,13 @@
 package card
 
 type Card struct {
-	Id         int64 `db:"id" json:"id"`                   // 主键自动增长id
-	CreateTime int64 `db:"create_time" json:"create_time"` // 创建时间
-	UpdateTime int64 `db:"update_time" json:"update_time"` // 更新时间
-	PersonId   string `db:"person_id" json:"person_id"`    // 身份ID
-	Cardno     string `db:"cardno" json:"cardno"`          // 卡号
-	Physicsno  string `db:"physicsno" json:"physicsno"`    // 物理号
-	Status     int8 `db:"status" json:"status"`            // 状态: -1删除  0：无效卡 1:入库  2：出库  3：未绑定  4：已绑定
+	Id         int64 `db:"id" json:"id"`                   // 主键
+	CreateTime int64 `db:"create_time" json:"create_time"` // 创建时间（毫秒）
+	UpdateTime int64 `db:"update_time" json:"update_time"` // 更新时间（毫秒）
+	CompanyId  int64 `db:"company_id" json:"company_id"`   // 企业ID
+	PersonId   int64 `db:"person_id" json:"person_id"`     // 身份ID
+	CardNo     string `db:"card_no" json:"card_no"`        // IC卡编号
+	PhysicsNo  string `db:"physics_no" json:"physics_no"`  // 物理编号
+	BatchNo    string `db:"batch_no" json:"batch_no"`      // 批次号
+	Status     int8 `db:"status" json:"status"`            // 状态 -1注销、0未出库、1已出库、2未绑定、3已绑定、4挂失
 }
---
+++ models/website/material.go
@@ -0,0 +1,14 @@
+package website
+
+type Material struct {
+	Id         int64 `db:"id" json:"id"`                   // 主键
+	CreateTime int64 `db:"create_time" json:"create_time"` // 创建时间（毫秒）
+	UpdateTime int64 `db:"update_time" json:"update_time"` // 修改时间（毫秒）
+	MenuId     int64 `db:"menu_id" json:"menu_id"`         // 菜单ID
+	Type       int8 `db:"type" json:"type"`                // 类型 应用 链接 图片 图文
+	Name       string `db:"name" json:"name"`              // 名称
+	Photo      string `db:"photo" json:"photo"`            // 图片
+	Url        string `db:"url" json:"url"`                // 链接
+	Desc       string `db:"desc" json:"desc"`              // 描述
+	Status     int8 `db:"status" json:"status"`            // 状态 -1注销 0正常
+}
