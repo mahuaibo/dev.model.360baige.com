ee67ba18a43688cf4d4884af0253c511c9af5973
mahuaibo <mahuaibo@360baige.com>
2017-09-05 15:22:51 +0800

    ma

--- models/account/status.go
+++ models/account/status.go
@@ -2,7 +2,7 @@ package account
 
 // 账户初始余额
 
-const AccountBalanceInit int64  = 0
+const AccountBalanceInit int64 = 0
 
 // 账户类型
 const (
@@ -12,6 +12,18 @@ const (
 
 // 账户状态
 const (
-	AccountStatusCancel = -1 // 正常
+	AccountStatusCancel = -1 // 销毁
 	AccountStatusNormal = 0  // 正常
+)
+
+// 账户记录状态
+const (
+	AccountItemStatusCancel = -1 // 销毁
+	AccountItemStatusNormal = 0  // 正常
+)
+
+// 交易状态
+const (
+	TransactionStatusCancel = -1 // 销毁
+	TransactionStatusNormal = 0  // 正常
 )
--- models/user/status.go
+++ models/user/status.go
@@ -1,5 +1,11 @@
 package user
 
+// 运营账号信息
+const UserPositionCompanyIdAudit int64 = 1000
+const UserIdAudit int64 = 1000
+const UserPositionTypeAudit int8 = 1
+const UserPositionIdAudit int64 = 1000
+
 // 用户状态
 const UserStatusCancel = -1
 const UserStatusNormal = 0
